75fe19a9e7d8017710fa056cd8d0eb991e25a720
John Agbanusi <agbanusijohn@gmail.com>
2026-05-07 15:04:20 +0100

    upgrade minter in feed test to include slashed amount

--- src/interfaces/IStPlumeMinter.sol
+++ src/interfaces/IStPlumeMinter.sol
@@ -20,6 +20,7 @@ interface IstPlumeMinter {
     function currentWithheldETH() external view returns (uint256);
     function totalInstantUnstaked() external view returns (uint256);
     function totalUnstaked() external view returns (uint256);
+    function slashedAmount() external view returns (uint256);
 
     // External Functions
     function submitForValidator(uint16 validatorId) external payable;
